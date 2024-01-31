22ca2d0688692c64af899eae377f3e39cbe41882
Mikjkj <144893796+Mikjkj@users.noreply.github.com>
2024-01-31 11:05:12 +0100

    Update helloworld.py

--- Web/helloworld.py
+++ Web/helloworld.py
@@ -4,4 +4,6 @@ from datetime import datetime
 import requests
 from flask import Flask, render_template
 
- r = requests.get(url=https://gjk.cz/)
+app = flask(__name__)
+app.render_template("hello")
+r = requests.get(url=https://gjk.cz/)
