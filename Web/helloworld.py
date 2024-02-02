244474a1661968d59a8a45d928208a0a1d3d0e2a
Mikjkj <144893796+Mikjkj@users.noreply.github.com>
2024-02-02 21:14:34 +0100

    Update helloworld.py

--- Web/helloworld.py
+++ Web/helloworld.py
@@ -1,9 +1,7 @@
 import random
-from datetime import datetime
-
 import requests
 from flask import Flask, render_template
 
 app = flask(__name__)
 app.render_template("hello")
-r = requests.get(url=https://gjk.cz/)
+
