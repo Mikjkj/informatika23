7ebe8c41648bde71c903df06dd35cfb85b3f11aa
Mikjkj <144893796+Mikjkj@users.noreply.github.com>
2024-02-21 10:42:16 +0000

    clicker

--- Web/helloworld.py
+++ Web/helloworld.py
@@ -1,16 +1,26 @@
 import random
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 count = 0
+power = 1
 app = Flask(__name__)
 
 @app.route("/")
-
 def protocol():
+    global count 
     pozdravy = ["ŽĎÁREC PÁREC!","HELLo","[VLOZ POZDRAV] ","DOBRÝ DEN, VÍTEJTE NA TÉTO STRÁNCE"]
     pozdrav = random.choice(pozdravy)
-    return render_template("hello.html", pozdrav = pozdrav)
+    return render_template("hello.html",count = count, pozdrav = pozdrav)
 
-@app.route("/add")
+@app.route("/add", methods = ["POST"])
 def add():
-    count+=1
+    global count, power
+    count = count + power
+    return redirect(url_for('protocol'))
+@app.route("/buy", methods = ["POST"])
+def buy():
+    global count, power
+    if count >= 10:
+        power += 1
+        count -= 10
+    return redirect(url_for('protocol'))
